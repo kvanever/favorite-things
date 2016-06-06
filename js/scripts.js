6eb4c5f0fb39ad28a3f381f10ef142c57dfee0d2
Kevin VanEvery and Matthew Reyes <matthew@>
2016-06-06 09:12:21 -0700

    adds javascript for manipulating array using loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
-/*This file is for your custom js.  All yours*/
+
+
+
+$(function() {
+  $("form#favorite-things").submit(function(event){
+    event.preventDefault();
+
+    var favoriteThings = [$("input#favorite-food").val(), $("input#favorite-tv").val(), $("input#favorite-restaurant").val(), $("input#favorite-book").val()];
+    var certainThings = [favoriteThings[1], favoriteThings[0], favoriteThings[2]];
+
+    debugger;
+
+    var pushThings = function(array1, array2) {
+
+      array1.forEach(function(item) {
+        array2.push(item);
+      });
+
+      array2.forEach(function(item){
+        $(".output ul").append("<li>" + item + "</li>");
+      });
+    }
+    pushThings(favoriteThings, certainThings);
+    alert(certainThings);
+  });
+});
